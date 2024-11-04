dd3111a66bdfbb1a70a23228e71aeb0784b26158
YESHALA AJAY KUMAR <ajayyeshala2002@gmail.com>
2024-11-04 10:14:39 +0000

    Initial commit

---
+++ src/main/java/com/example/artgallery/repository/GalleryRepository.java
@@ -0,0 +1,27 @@
+/*
+ * You can use the following import statements
+ *
+ * import java.util.ArrayList;
+ * import java.util.List;
+ * 
+ */
+
+package com.example.artgallery.repository;
+
+import java.util.ArrayList;
+import java.util.List;
+import com.example.artgallery.model.*;
+
+public interface GalleryRepository {
+    ArrayList<Gallery> getGalleries();
+
+    Gallery getGalleryById(int galleryId);
+
+    Gallery addGallery(Gallery gallery);
+
+    Gallery updateGallery(int galleryId, Gallery gallery);
+
+    void deleteGallery(int galleryId);
+
+    ArrayList<Artist> getArtistsByGalleryId(int galleryId);
+}
